ab02f69306220c66ddf6b44fd9a7ef6eeeee4707
Peter MOUEZA <mouezapeter@gmail.com>
2018-04-26 01:46:26 +0200

    mvn test OK. Eclipse JUnit test KO (JUnit Runner error

---
+++ mvntest/src/test/java/homniserv/moueza/mvntest/AppTest.java
@@ -0,0 +1,18 @@
+package homniserv.moueza.mvntest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void testAdd() {
+		// App app = new App();
+		// Integer res = app.add(1, 2);
+		// assertSame(3, res);
+		// assertTrue(3 == res);
+		assertEquals(2, 1 + 1);/** https://junit.org/junit5/docs/current/user-guide/#dependency-diagram */
+	}
+
+}
