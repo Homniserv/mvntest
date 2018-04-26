1e0783e21d670f9887a8dd48ec0bde3331ce8f89
Peter MOUEZA <mouezapeter@gmail.com>
2018-04-26 04:08:32 +0200

    sov

---
+++ mvntest/src/test/java/homniserv/moueza/mvntest/AppTest3noBaseClass.java
@@ -0,0 +1,19 @@
+package homniserv.moueza.mvntest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+
+class AppTest3noBaseClass {
+
+	@Test
+	void testAdd() {
+		assertEquals("My", "My" );
+
+	}
+
+}
